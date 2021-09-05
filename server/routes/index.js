6100602e6d73506c9600c08181847d4ef0a9808f
BRASS <mr.brn.lewis@outlook.com>
2021-09-05 16:16:10 +0800

    routing front end and begind model setup

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,14 @@ router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../public/index.html"))
 });
 
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/exercise.html"))
+});
 
-  
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/stats.html"))
+});
 
-router.use('/api/exercise', exerciseRoutes);
 
 module.exports = router;
 
